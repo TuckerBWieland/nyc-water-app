ef5f9245daa83dab5b2dd061b33dc46b0c271cfa
Tucker <wieland.tucker@gmail.com>
2025-05-24 15:26:13 -0400

    feat: dynamically import posthog

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,4 +1,5 @@
-import posthog from 'posthog-js';
+// Dynamically imported PostHog instance
+let posthog;
 
 export const EVENT_CLICK_SITE_MARKER = 'click_site_marker';
 export const EVENT_CLICK_DATA_INFO_BUTTON = 'click_data_info_button';
@@ -8,11 +9,14 @@ export const EVENT_OPEN_POPUP = 'open_popup';
 
 let isInitialized = false;
 
-export function initAnalytics() {
+export async function initAnalytics() {
   const key = import.meta.env.VITE_POSTHOG_KEY;
   if (!key || isInitialized) {
     return;
   }
+
+  const mod = await import('posthog-js');
+  posthog = mod.default || mod;
 
   posthog.init(key, {
     api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://app.posthog.com',
